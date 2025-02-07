5e2acc9741ea0f532b3f83815206a84d2b91184e
arhamfareed106 <arhamfareed575@gmail.com>
2025-02-07 17:25:19 +0500

    main app

--- SaaS/urls.py
+++ SaaS/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('format.urls')),  # Include app's URLs
+    path('', include('format.urls')),     # Include app's URLs
 ]
-
