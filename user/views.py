3db349d4ac096e75700ad8dde5dee84ba92db0ab
arhamfareed106 <arhamfareed575@gmail.com>
2025-02-23 05:27:45 +0500

    user error

--- user/views.py
+++ user/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from dataclasses import field
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from .models import User
 
 # Create your views here.
 
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        field = ['username','email', 'password1', 'password2']
+
 
 def signup(request):
-    return render(request, 'user/signup.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            user= form.save()
+            authenticate(username=user.username, password=user.password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+            
+    else:
+        form = SignupForm()
+    return render(request, 'user/signup.html',{
+        'form': form,
+    })
+
